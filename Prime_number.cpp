9a862e864cf23a57f549b8bc19668b091f4aa9df
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-05-12 23:14:12 +0530

    Add files via upload

---
+++ Prime_number.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n, int i = 2)
+{
+	// Base cases
+	if (n <= 2)
+		return (n == 2) ? true : false;
+	if (n % i == 0)
+		return false;
+	if (i * i > n)
+		return true;
+
+	// Check for next divisor
+	return isPrime(n, i + 1);
+}
+
+int main()
+{
+	int n = 15;
+	if (isPrime(n))
+		cout << "Yes";
+	else
+		cout << "No";
+
+	return 0;
+}
