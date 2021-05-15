67cbcf3bb6ed9b25ac20cabc13c565b341a0126a
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-05-15 23:26:33 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+public class Main
+{
+ 
+  // Factorial function
+  static int f(int n)
+  {
+ 
+    // base condition
+    if (n == 0 || n == 1)
+      return 1;
+ 
+    // Recursive condition
+    else
+      return n * f(n - 1);
+  }
+ 
+  /
+  public static void main(String[] args)
+  {
+    int n = 5;
+    System.out.println("factorial of " + n + " is: " + f(n));
+  }
+}
